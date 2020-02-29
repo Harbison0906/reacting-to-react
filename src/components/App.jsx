7b7d337d794a77b036545a7805a6c29a32408aa4
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-02-29 13:17:22 -0600

    changed functional component to class component; initialized state; added input field and class method to update state to whatever input value is

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
-const App = (props) => {
-  return ( <h1>Hey! My name is Seth. What's yours?</h1> )
+// const App = (props) => {
+//   return ( <h1>Hey! My name is { props.name }. What's yours?</h1> )
+// }
+
+class App extends Component {
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      text: 'React Lab'
+    }
+  }
+
+  inputChange = (text) => {
+    this.setState({ text });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hey! My name is {this.props.name}. What's yours?</h1>
+        <p>{this.state.text}</p>
+        <input
+          placeholder={this.state.text}
+          onChange={(event) => {this.inputChange(event.target.value) }}
+        />
+      </div>
+
+    );
+  }
+
 }
-
 export default App;
